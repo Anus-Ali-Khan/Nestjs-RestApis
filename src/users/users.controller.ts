c3ba82e8e0077001e936c656467b40399bcdc8a1
Anus Ali Khan <anusalikhan1405@gmail.com>
2025-01-01 11:28:37 +0500

    feat: dto and pipes in nest js

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,8 +7,12 @@ import {
   Patch,
   Post,
   Query,
+  ParseIntPipe,
+  ValidationPipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -24,37 +28,29 @@ export class UsersController {
 
   //Since this is a dynamic route so this will be placed at the end of all the static routes otherwise nest will read all the static routes a s dynamic
   @Get(':id') //GET /users/:id
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: number) {
     //params are always in string
-    return this.usersService.findOne(+id); // Unary plus(+) converts string into number
+    return this.usersService.findOne(id); // Unary plus(+) converts string into number
   }
 
   @Post() //POST /users
   create(
-    @Body()
-    user: {
-      name: string;
-      email: string;
-      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
-    },
+    @Body(ValidationPipe)
+    createUserDto: CreateUserDto,
   ) {
-    return this.usersService.create(user);
+    return this.usersService.create(createUserDto);
   }
 
   @Patch(':id') //PATCH /users/:id
   update(
-    @Param('id') id: string,
-    @Body()
-    userUpdate: {
-      name?: string;
-      email?: string;
-      role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
-    },
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe)
+    updateUserDto: UpdateUserDto,
   ) {
-    return this.usersService.update(+id, userUpdate);
+    return this.usersService.update(id, updateUserDto);
   }
   @Delete(':id') //DELTE /users/:id
-  delete(@Param('id') id: string) {
-    return this.usersService.delete(+id);
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.delete(id);
   }
 }
